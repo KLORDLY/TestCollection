64e40e8a69b70bbc138ea50f3907aea04bbecd1a
KLORDLY <179728691@qq.com>
2019-06-13 00:45:58 +0800

    修改AsyncBindingTest

--- AsyncBindingTest/Form1.cs
+++ AsyncBindingTest/Form1.cs
@@ -12,9 +12,20 @@ namespace AsyncBindingTest
 {
     public partial class Form1 : Form
     {
+        DataSource dataSource = new DataSource();
         public Form1()
         {
             InitializeComponent();
+            AsyncBindingHelper.AddBinding(lblValue, "Text", (INotifyPropertyChanged)dataSource,"Value");
+           
         }
+
+        int a = 0;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            a++;
+            dataSource.Value = a.ToString();
+        }
+
     }
 }
